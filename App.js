326758ce6769fa8c33bfd75fa87f18f0124d6b79
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2024-03-22 13:10:09 +0600

    Starting page added

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import LoginScreen from './Apps/Screens/LoginScreen';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-white">Hello world!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-white">
+      <Image source={require('./assets/Images/starting-page.jpg')} 
+        className="w-full h-[400px]"
+      />
+      <View className="p-8">
+        <Text className="font-bold text-[30px]">Community Markteplace</Text>
+        <Text className="text-slate-500 mt-6 text-[18px]">Sell your old item and make money</Text>
+      </View>
     </View>
   );
 }
