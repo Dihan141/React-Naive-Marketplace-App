3794fb1d83e7283df3da2c5a0d9cf313ae2adeb3
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2024-03-22 15:06:57 +0600

    Added clerk auth

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
+import { Text, View } from 'react-native';
 import LoginScreen from './Apps/Screens/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white">
-      <Image source={require('./assets/Images/starting-page.jpg')} 
-        className="w-full h-[400px]"
-      />
-      <View className="p-8">
-        <Text className="font-bold text-[30px]">Community Markteplace</Text>
-        <Text className="text-slate-500 mt-6 text-[18px]">Sell your old item and make money</Text>
+    <ClerkProvider publishableKey='pk_test_bmF0aXZlLWZsYW1pbmdvLTE1LmNsZXJrLmFjY291bnRzLmRldiQ'>
+      <View className="flex-1 bg-white">
+        <StatusBar style="auto"/>
+        <SignedIn>
+          <Text className="mt-10">You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
       </View>
-    </View>
+    </ClerkProvider>
   );
 }
--- Apps/Screens/LoginScreen.jsx
+++ Apps/Screens/LoginScreen.jsx
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import React from 'react'
+import * as WebBrowser from "expo-web-browser"
+import { useOAuth } from "@clerk/clerk-expo"
+import { useWarmUpBrowser } from "../../hooks/useWarmUpBrowser"
 
+WebBrowser.maybeCompleteAuthSession()
 export default function LoginScreen() {
+  useWarmUpBrowser();
+ 
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Image source={require('../../assets/Images/starting-page.jpg')} 
+        className="w-full h-[400px] object-cover"
+      />
+      <View className="p-8">
+        <Text className="font-bold text-[30px]">Community Markteplace</Text>
+        <Text className="text-slate-500 mt-6 text-[18px]">Sell your old item and make money</Text>
+        <TouchableOpacity onPress={onPress} className="p-3 bg-blue-500 rounded-full mt-20">
+          <Text className="text-white text-center text-[16px]">Get Started</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
