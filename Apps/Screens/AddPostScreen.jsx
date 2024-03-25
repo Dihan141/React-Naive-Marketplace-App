0073c06c269f8424a41d9fafedf2b31ccc49c12d
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2024-03-25 16:39:07 +0600

    Adding new item add form with formik

--- Apps/Screens/AddPostScreen.jsx
+++ Apps/Screens/AddPostScreen.jsx
@@ -1,19 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Touchable, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { app } from '../../firebaseConfig'
 import { getFirestore, collection, query, getDocs } from 'firebase/firestore'
+import { Formik } from 'formik'
 
 export default function AddPostScreen() {
     const db = getFirestore(app)
     const [categoryList, setCategoryList] = useState([])
 
     const getCategories = async () => {
+      setCategoryList([])
         const queryString = query(collection(db, 'Category'))
 
         const querySnapshot = await getDocs(queryString)
         
         querySnapshot.forEach((doc) => {
-            console.log(doc)
             console.log(doc.data())
             setCategoryList(categoryList => [...categoryList, doc.data])
         })
@@ -23,8 +24,60 @@ export default function AddPostScreen() {
         getCategories()
     }, [])
   return (
-    <View>
-      <Text>AddPostScreen</Text>
+    <View className="p-10">
+      <View>
+        <Text className="text-[27px] font-bold">Add New Item</Text>
+        <Text className="text-gray-500 mb-5">Add new item and start selling</Text>
+      </View>
+      <Formik
+        initialValues={{title:'', desc:'', category:'', address:'', price:'', image:''}}
+        onSubmit={value=>console.log(value)}
+      >
+        {({handleSubmit, handleBlur, handleChange, values}) => (
+          <View>
+            <TextInput 
+              placeholder='Title' 
+              className="border rounded-[10px] w-full p-3 mt-2 mb-3"
+              value={values?.title}
+              onChangeText={handleChange('title')}
+            />
+            <TextInput 
+              placeholder='Description' 
+              className="border rounded-[10px] w-full p-3 mt-2 mb-3"
+              style={styles.input}
+              value={values?.desc}
+              numberOfLines={5}
+              onChangeText={handleChange('desc')}
+            />
+            <TextInput 
+              placeholder='Address' 
+              className="border rounded-[10px] w-full p-3 mt-2 mb-3"
+              value={values?.address}
+              onChangeText={handleChange('address')}
+            />
+            <TextInput 
+              placeholder='Price' 
+              className="border rounded-[10px] w-full p-3 mt-2 mb-3"
+              value={values?.price}
+              keyboardType='number-pad'
+              onChangeText={handleChange('price')}
+            />
+
+            <TouchableOpacity 
+              onPress={handleSubmit}
+              className="bg-blue-500 p-4 mt-5 rounded-full"
+            >
+              <Text className="text-white text-center">Submit</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input:{
+    textAlignVertical:'top'
+  }
+})
